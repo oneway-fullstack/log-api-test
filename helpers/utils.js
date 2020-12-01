e9da4c0afd47d2c4d183f7da529de77d05d479ca
TrustBiz <60724108+ChenSeeSai@users.noreply.github.com>
2020-12-01 21:54:07 +0800

    cache duplication

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,5 +1,8 @@
 const fetch = require('node-fetch');
 const settings = { method: "Get" };
+const NodeCache = require('node-cache');
+const ttl = 60 * 5 * 1; // cache for 5mins
+const my_cache = new NodeCache({stdTTL: ttl, checkperiod: 120});
 
 exports.getJsonPlaceHolderUri = (end_point) => {
     const uri = `${process.env.JSON_PLACEHOLDER_URI}/${end_point}/` || `https://jsonplaceholder.typicode.com/${end_point}/`;
@@ -7,15 +10,27 @@ exports.getJsonPlaceHolderUri = (end_point) => {
 }
 
 exports.getInfo = async (id) => {
-    const album_uri = await this.getJsonPlaceHolderUri('albums') + id;
-    const album_res = await fetch(album_uri, settings).then(res => res.json());
+    let album_res = my_cache.get('album_' + id)
+    if ( album_res == undefined ) {
+        const album_uri = await this.getJsonPlaceHolderUri('albums') + id;
+        album_res = await fetch(album_uri, settings).then(res => res.json());
+    }
 
-    const user_uri = await this.getJsonPlaceHolderUri('users') + album_res.userId;
-    const user_res = await fetch(user_uri, settings).then(res => res.json());
+    my_cache.set('album_' + id, album_res);
+
+    const user_id = album_res.userId;
+
+    let user_res = my_cache.get('user_' + user_id);
+    if ( user_res == undefined ) {
+        const user_uri = await this.getJsonPlaceHolderUri('users') + user_id;
+        user_res = await fetch(user_uri, settings).then(res => res.json());
+    }
+
+    my_cache.set('user_'+ user_id, user_res);
 
     let result = {
         id: id,
-        user_id: album_res.userId,
+        user_id: user_id,
         title: album_res.title,
         name: user_res.username
     };
